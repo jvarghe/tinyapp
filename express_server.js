ab3a77900fda45ab3c0b6765cc1dd984e6857b56
jvarghe <vcs.inbox@outlook.com>
2023-06-27 22:28:08 +0000

    Add '/urls/:id' endpoint to 'express_server.js'. Also add its associated view, 'urls_show.ejs' to the 'views' directory.

--- express_server.js
+++ express_server.js
@@ -13,7 +13,6 @@ const PORT = 8080;
 app.set("view engine", "ejs");
 
 
-
 // Added an object with a list of URLs to the project. It simulates a data
 // source like a database.
 const urlDatabase = {
@@ -47,10 +46,42 @@ app.get("/urls", (req, res) => {
 
   // If you are sending data to a view, even a single variable, the convention
   // is to wrap it in an object called `templateVars`.
-  const templateVariables = { urls: urlDatabase };
+  // const templateVariables = { urls: urlDatabase };
 
-  // Return the `urls_index.ejs` template. Embed `urlDatabase` in it.
+  // Return the `urls_index.ejs` template. Embed values from`urlDatabase` in it.
   res.render("urls_index.ejs", templateVariables);
+
+});
+
+
+// Route handler for GET `/urls:id`.
+
+ /* ROUTE PARAMETERS
+  *
+  * If you start the server and go to `http://localhost:8080/urls/LHLabs`,
+  * then `LHLabs` is the value of `:id` Route Parameter. It gets passed in
+  * via the URL and is added to the request body. When the request arrives at
+  * the backend, Express.js loads this value into the `req.params.id` property.
+  * From there, you can load it into the view, where it can be used.
+  */
+app.get("/urls/:id", (req, res) => {
+
+  // Extract the `:id` value from the request object. You can find it in the
+  // `request.params.id` property.
+  const templateVariables = { id: req.params.id,
+    longURL: urlDatabase[req.params.id] };
+
+  // If you start the server, go its webpage and go to `.../urls/value1` and
+  // pass in `b2xVn2`, the line below will print out the `templateVariable`
+  // object's elements, like this:
+  //
+  //     { id: 'b2xVn2', longURL: 'http://www.lighthouselabs.ca' }
+  //
+  // console.log(templateVariables);
+
+  // Invoke the template engine, ask for the view called `urls_show.ejs` and
+  // pass in the `templateVariables` object. Embed values from it in the view.
+  res.render("urls_show.ejs", templateVariables);
 
 });
 
